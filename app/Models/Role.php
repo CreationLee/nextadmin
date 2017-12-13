5ed7add608eea7434b414f2095003d2de710ce06
leekunpc <leekun030@163.com>
2017-12-13 19:51:35 +0800

    更改

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,10 +9,6 @@ class Role extends Model
 {
     protected $guarded = [];
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b6039cf93e1f8404a815feb56fed0e359be33b4d
     public function permissions()
     {
         return $this->belongsToMany(AdminFacades::modelClass('Permission'));
